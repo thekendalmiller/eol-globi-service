c70911f2d52098eed1fbdee4018ed8c91e9be971
jhpoelen <jhpoelen@CentOS-63-64-minimal.(none)>
2013-02-13 20:14:50 +0100

    remove reference to monit

--- upgrade.sh
+++ upgrade.sh
@@ -1,15 +1,10 @@
 # used to upgrade server on aws
-
-sudo monit quit
-sudo service neo4j-service remove
+service neo4j-service stope
 
 git reset --hard
 git clean -f -d 
 
 git pull --rebase
 
-sudo sh neo4j-community-1.9.M03/bin/neo4j install -u ec2-user -h
-
 service neo4j-service start
 
-sudo monit -d 60
